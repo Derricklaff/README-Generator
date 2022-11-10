50989c0e9e25df5c3a0048b93d051b5a1cd0357f
Derrick Lafforthun <derricklaff@gmail.com>
2022-11-09 19:28:33 -0700

    revising generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,22 +2,31 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
-    case 'MIT':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    case 'Apache 2.0':
-      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-    case 'Mozilla':
-      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
-    case 'none':
-      return '---'
-    default:
-      break;
+    case "MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case "Apache":
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    case "Mozilla":
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    case "none":
+      return ``;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return `https://opensource.org/licenses/MIT`;
+    case "Apache":
+      return `https://opensource.org/licenses/Apache-2.0`;
+    case "Mozilla":
+      return `https://opensource.org/licenses/MPL-2.0`;
+    case "none":
+      return ``;
+  }
+}
 
 // // TODO: Create a function that returns the license section of README
 // // If there is no license, return an empty string
